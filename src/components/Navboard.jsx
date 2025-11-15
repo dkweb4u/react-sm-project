23b5dd1f1c2cf4d6c7c89bd7115aed009d018bfe
Dinesh Kumar <126790764+dkweb4u@users.noreply.github.com>
2025-11-15 17:04:19 +0530

    update

--- src/components/Navboard.jsx
+++ src/components/Navboard.jsx
@@ -10,7 +10,7 @@ export const Navboard = ({category}) => {
     
       fetch(url)
       .then(res=>res.json()).
-      then(data => {setArticle(data.articles);});
+      then(data => {setArticle(data.articles);console.log(data)});
     
     },[category]);
 
